abd55f42d8ce5b1896e0dde6a6b1ff955df12290
VslVictor7 <vic.collfac358@outlook.com>
2024-12-10 15:12:42 -0300

    chore: bot can now run

---
+++ bot/main.py
@@ -0,0 +1,55 @@
+import discord
+import os
+import aiohttp
+from scripts.mybot import MyBot
+from scripts.birthday_database import create_birthday_data
+from scripts.birthday_checker import parse_birthdays, birthday_check_periodically
+from commands import setup_commands
+from dotenv import load_dotenv
+
+load_dotenv()
+
+TOKEN = os.getenv('DISCORD_TOKEN')
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
+MESSAGE_ID = int(os.getenv('MESSAGE_ID'))
+FRIENDS_BIRTHDAYS = os.getenv('BIRTHDAYS')
+
+# Rodar o bot.
+
+bot = MyBot()
+
+@bot.event
+async def on_ready():
+
+    print(f"[BOT] Logado como {bot.user.name} - {bot.user.id}")
+
+    activity = discord.Activity(type=discord.ActivityType.watching, name="Quem são os aniveriantes!")
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+    await background_tasks()
+
+    create_birthday_data()
+
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        try:
+            parsed_birthdays = parse_birthdays(FRIENDS_BIRTHDAYS)
+            print("[BOT STARTED] Pronto para monitoramento de Aniversariantes.")
+
+            await birthday_check_periodically(bot, parsed_birthdays)
+
+        except discord.DiscordException as e:
+            print(f"[BOT ERROR] Erro ao buscar mensagem: {e}")
+            await bot.close()
+    else:
+        print("[BOT ERROR] Canal não detectado.")
+        await bot.close()
+
+
+async def background_tasks():
+    await bot.wait_until_ready()
+
+    bot.loop.create_task(setup_commands(bot))
+    bot.loop.create_task(bot.sync_commands())
+
+bot.run(TOKEN)
